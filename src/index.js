7c3b186d1e70357cc0d8b37a5441952425992abc
Biradar542 <shubhambiradar721@gmail.com>
2024-12-08 14:21:45 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-/* global document */
+import { combineReducers } from "redux";
 
-import React from "react";
-import { render } from "react-dom";
-import { Provider } from "react-redux";
+import weatherStation from "./weatherStation";
 
-import App from "./App";
-import store from "./store";
-
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>, document.getElementById("root")
-);
+export default combineReducers({ weatherStation });
